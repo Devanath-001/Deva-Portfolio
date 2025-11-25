a888f37939fc5bd4c0a3e93fffb0a7a737225fa4
Devanath <repanadevanath796@gmail.com>
2025-11-25 12:34:12 +0530

    Updated media queries and animations

--- main.js
+++ main.js
@@ -8,12 +8,11 @@ var typed = new Typed(".text", {
 });
 
 
-/* ⭐ Navigation Toggle కోసం కొత్త JS కోడ్ ⭐ */
 
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 
 menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x'); // క్లిక్ చేసినప్పుడు menu icon 'x' గా మారుతుంది
-    navbar.classList.toggle('active');  // navbar కు 'active' class జోడించబడుతుంది
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 };
